865a60076253ebf2c4bd00b32e9c2436cefea0b0
carthebottlecold <qudcks139@daum.net>
2025-01-21 02:24:35 +0900

    시간 표시 변경
    - 시간 표시 오류 변경
      => defined  => 방금 전
    - image 받는 형식 변경

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ con = sqlite3.connect('db.db',check_same_thread=False)
 cur = con.cursor()
 
 cur.execute(f"""
-            CREATE TABLE items (
+            CREATE TABLE IF NOT EXISTS items (
                 id INTEGER PRIMARY KEY,
                 title TEXT NOT NULL,
                 image BLOB,
@@ -58,7 +58,7 @@ async def get_image(item_id):
                             SELECT image from items WHERE id={item_id}
                             """).fetchone()[0]
   
-  return Response(content=bytes.fromhex(image_bytes))
+  return Response(content=bytes.fromhex(image_bytes),media_type='image/*')
 
 
 app.mount("/", StaticFiles(directory="frontend",html=True), name="frontend")
